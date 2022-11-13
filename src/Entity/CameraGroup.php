901ac82f1abeeb28a27548b131bbe9f403a81d1a
BrietGame <alexis.briet2003@gmail.com>
2022-11-13 19:35:11 +0100

    Apply ApiRessource

--- src/Entity/CameraGroup.php
+++ src/Entity/CameraGroup.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\CameraGroupRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -9,6 +10,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CameraGroupRepository::class)]
+#[ApiResource]
 class CameraGroup
 {
     #[ORM\Id]

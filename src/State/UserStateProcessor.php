dcf38d94b21115ce48e2e5f25a93e6dd96952970
BrietGame <alexis.briet2003@gmail.com>
2022-11-14 19:32:11 +0100

    Register : add register endpoint with hash pwd

---
+++ src/State/UserStateProcessor.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProcessorInterface;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserStateProcessor implements ProcessorInterface
+{
+    private EntityManagerInterface $entityManager;
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->entityManager = $entityManager;
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function supports($data): bool
+    {
+        return $data instanceof User;
+    }
+
+
+
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): void
+    {
+        // Handle the state
+        if ($data->getPassword()) {
+            $data->setPassword(
+                $this->passwordHasher->hashPassword($data, $data->getPassword())
+            );
+            $data->eraseCredentials();
+        }
+        $this->entityManager->persist($data);
+        $this->entityManager->flush();
+    }
+
+    public function remove($data)
+    {
+        $this->entityManager->remove($data);
+        $this->entityManager->flush();
+    }
+}

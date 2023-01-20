54bff9d1a2461be195e1ec689f96a041facaad85
BrietGame <alexis.briet2003@gmail.com>
2023-01-20 20:58:13 +0100

    16-get-overlays1 : show email

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -97,7 +97,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $password = null;
 
     #[ORM\Column(length: 255, unique: true)]
-    #[Groups(['user:read', 'user:write'])]
+    #[Groups(['user:read', 'user:write','overlay:read'])]
     private ?string $email = null;
 
     #[ORM\OneToMany(mappedBy: 'userOwner', targetEntity: Overlay::class)]

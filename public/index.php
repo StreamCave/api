4c8b546cd2d7eb98c05bcd20d97c2e0dc736e863
Alexis BRIET <alexis.briet2003@gmail.com>
2023-02-20 18:46:21 +0100

    Fix CORS 2

--- public/index.php
+++ public/index.php
@@ -5,12 +5,5 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    header('Access-Control-Allow-Headers: Content-Type, Authorization');
-    header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Expose-Headers: Content-Disposition');
-    header('Access-Control-Max-Age: 86400');
-    header('Content-Type: application/json');
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };

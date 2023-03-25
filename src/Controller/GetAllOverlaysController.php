89b79108a387ed0dc106decf49395bf115b607e1
Alexis BRIET <alexis.briet2003@gmail.com>
2023-03-25 18:18:35 +0100

    Get AllOverlays with JWT Token

--- src/Controller/GetAllOverlaysController.php
+++ src/Controller/GetAllOverlaysController.php
@@ -7,18 +7,23 @@ use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class GetAllOverlaysController extends AbstractController
 {
-    public function __construct(OverlayRepository $overlayRepository, UserRepository $userRepository)
+    public function __construct(TokenStorageInterface $tokenStorageInterface, JWTTokenManagerInterface $jwtManager, OverlayRepository $overlayRepository, UserRepository $userRepository)
     {
         $this->overlayRepository = $overlayRepository;
         $this->userRepository = $userRepository;
+        $this->tokenStorageInterface = $tokenStorageInterface;
+        $this->jwtManager = $jwtManager;
     }
 
     public function __invoke(Request $request, $uuid): Response
     {
-        $user = $this->userRepository->findOneBy(['uuid' => $uuid]);
+        $decodedJwtToken = $this->jwtManager->decode($this->tokenStorageInterface->getToken());
+        $user = $this->userRepository->findOneBy(['uuid' => $decodedJwtToken['uuid']]);
         $overlays = $this->overlayRepository->findAllAccess($user->getUuid());
 
         return $this->json([

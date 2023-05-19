e46d9fd62606a1fdc17d2ec03f4888d2c4562341
Alexis BRIET <alexis.briet2003@gmail.com>
2023-05-19 19:32:22 +0200

    Update her6s fixtures

---
+++ src/DataFixtures/models/her6s/GroupFixtures.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\DataFixtures\models\her6s;
+
+use App\DataFixtures\MapsFixtures;
+use App\DataFixtures\OverlayFixtures;
+use App\Entity\AnswerGroup;
+use App\Entity\CameraGroup;
+use App\Entity\InfoGroup;
+use App\Entity\MapGroup;
+use App\Entity\MatchGroup;
+use App\Entity\PlanningGroup;
+use App\Entity\PollGroup;
+use App\Entity\PopupGroup;
+use App\Entity\TweetGroup;
+use App\Repository\LibMapRepository;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Uid\Uuid;
+
+class GroupFixtures extends Fixture implements DependentFixtureInterface
+{
+    private const MODEL = 'her6s';
+
+    public function __construct(LibMapRepository $libMapRepository)
+    {
+        $this->libMapRepository = $libMapRepository;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $this->setCameraGroup($manager);
+        $this->setInfoGroup($manager);
+    }
+
+    private function setCameraGroup(ObjectManager $manager): void
+    {
+        $players = ['Alpha', 'Bravo', 'Charlie', 'Delta', 'Echo', 'Foxtrot', 'Golf', 'Hotel', 'India', 'Juliett'];
+
+        foreach ($players as $key => $player) {
+            $camera = new CameraGroup();
+            $camera->setUuid(Uuid::v5(Uuid::v6(), "Camera $player"));
+            $camera->setName("Camera $player");
+            $camera->setVisible(false);
+            $camera->setSocketId("socket-$player");
+            $this->addReference("camera-group-" . self::MODEL . "-$player", $camera);
+
+            $manager->persist($camera);
+            $manager->flush();
+        }
+    }
+
+    private function setInfoGroup(ObjectManager $manager): void
+    {
+        $group = new InfoGroup();
+        $group->setUuid(Uuid::v5(Uuid::v6(), 'Info FlowUp'));
+        $group->setTitre('#SaltyDuels');
+        $group->setLogo('https://cdn.streamcave.tv/models/flowup/FlowUpSigneWhite.svg');
+        $group->setDescription('Groupe info des Salty Duels');
+        $group->setTextScroll(['Bienvenue sur les SaltyDuels by FlowUp !', 'Sixquatre assure le Cast de ce tournoi R6 en compagnie de StreamCave et de FlowUp.', 'Bonne chance à tous !']);
+
+        $this->addReference('info-group-' . self::MODEL, $group);
+
+        $manager->persist($group);
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            MapsFixtures::class,
+            OverlayFixtures::class
+        ];
+    }
+}
---
+++ src/DataFixtures/models/her6s/WidgetFixtures.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\DataFixtures\models\her6s;
+
+use App\DataFixtures\OverlayFixtures;
+use App\Entity\Widget;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Uid\Uuid;
+
+class WidgetFixtures extends Fixture implements DependentFixtureInterface
+{
+    private const MODEL = 'her6s';
+
+    public function load(ObjectManager $manager): void
+    {
+        $this->setTopBar($manager);
+        $this->setCameras($manager);
+    }
+
+    private function setTopBar(ObjectManager $manager): void
+    {
+        $widget = new Widget();
+        $widget->setUuid(Uuid::v5(Uuid::v6(), 'TopBar'));
+        $widget->setName('TopBar');
+        $widget->setDescription('Barre en haut de page.');
+        $widget->setVisible(false);
+        $widget->setInfoGroup($this->getReference('info-group-' . self::MODEL));
+        $widget->addMatchGroup($this->getReference('match-group-' . self::MODEL));
+        $widget->setOverlay($this->getReference('overlay-' . self::MODEL));
+
+        $manager->persist($widget);
+        $manager->flush();
+    }
+    private function setCameras(ObjectManager $manager): void
+    {
+        $players = ['Alpha', 'Bravo', 'Charlie', 'Delta', 'Echo', 'Foxtrot', 'Golf', 'Hotel', 'India', 'Juliett'];
+        $widget = new Widget();
+        $widget->setUuid(Uuid::v5(Uuid::v6(), 'Cameras'));
+        $widget->setName('Cameras');
+        $widget->setDescription('Cameras.');
+        $widget->setVisible(false);
+        $widget->addCameraGroup($this->getReference('camera-group-' . self::MODEL .  '-' . $players[0]));
+        $widget->addCameraGroup($this->getReference('camera-group-' . self::MODEL .  '-' . $players[1]));
+        $widget->setOverlay($this->getReference('overlay-' . self::MODEL));
+
+        $manager->persist($widget);
+        $manager->flush();
+    }
+    public function getDependencies(): array
+    {
+        return array(
+            GroupFixtures::class,
+            OverlayFixtures::class,
+        );
+    }
+}
